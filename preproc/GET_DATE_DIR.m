01fd8f6431b02848f26978f26d12ed6db525a475
scorde <sebastien.corde@polytechnique.edu>
2013-05-19 11:33:56 -0700

    Removed preproc (conflicts with QPIC_LITE)

--- preproc/GET_DATE_DIR.m
+++
@@ -1,7 +0,0 @@
-function date_dir = GET_DATE_DIR()
-
-datstr = date;
-[day, monstr] = strtok(datstr,'-');
-[mon, yerstr] = strtok(monstr,'-');
-[yer, ~]      = strtok(yerstr,'-');
-date_dir      = [yer '/' mon '/' day '/'];
